cdd779a0cfdf6d29e91bbc02f31a48ed0f534749
Gregory Eales <gregoeales@aol.com>
2019-02-11 21:55:44 -0800

    Update Notes.py

--- Python/Object-Oriented-Programming/Object-Design/Notes.py
+++ Python/Object-Oriented-Programming/Object-Design/Notes.py
@@ -1,3 +1,4 @@
+import math 
 
 # basic class syntax
 class MyClass(object):
@@ -9,7 +10,43 @@ b = MyClass()
 print(a)
 print(b)
 
-a.x = 1
-a.y = 2
-b.x = 2
-b.y = 1
+
+class Point():
+	""" Represents a 2-D point"""
+
+	def __init__(self, x=0, y=0):
+		""" initialize the point """
+		self.x = x
+		self.y = y
+
+	def reset(self):
+		""" set the point back to the origin """
+		self.x = 0
+		self.y = 0
+
+	def move(self, x, y):
+		""" moves the point to x, y """
+		self.x = x
+		self.y = y
+
+	def calculate_distance(self, other_point):
+		""" calculates the distance between 
+		the point and another """
+		deltaX = (self.x - other_point.x)**2
+		deltaY = (self.y - other_point.y)**2
+		return math.sqrt(deltaY + deltaX)
+
+p1 = Point(3, 4)
+p2 = Point()
+print(p1.calculate_distance(p2))
+
+
+
+
+
+
+
+
+
+
+
