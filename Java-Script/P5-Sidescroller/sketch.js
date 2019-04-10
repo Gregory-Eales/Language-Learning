141cdf398f286d99e6e2c9bc41de1bf534d482af
Gregory Eales <gregoeales@aol.com>
2019-04-09 23:22:07 -0700

    small changes

--- Java-Script/P5-Sidescroller/sketch.js
+++ Java-Script/P5-Sidescroller/sketch.js
@@ -1,13 +1,16 @@
 var back_img;
 
 function setup() {
-    createCanvas(1000, 1000);
-    back_img = loadImage('assets⁩/back.png');
+    createCanvas(600, 600);
+    //back_img = loadImage('assets⁩/back.png');
 }
 
 function draw() {
     
-    image(back_img, 0, 0, 1000, 1000);
+    
+    background(0);
+    rect(1, 2, 20, 10);
+    //image(back_img, 0, 0, 1000, 1000);
 }
 
 
