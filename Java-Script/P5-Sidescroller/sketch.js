eec0cc45cd44e203dcbb982b2ecf022d055e6541
Gregory Eales <gregoeales@aol.com>
2019-04-09 18:42:18 -0700

    added game assets

---
+++ Java-Script/P5-Sidescroller/sketch.js
@@ -0,0 +1,13 @@
+var back_img;
+
+function setup() {
+    createCanvas(1000, 1000);
+    back_img = loadImage('assets⁩/back.png');
+}
+
+function draw() {
+    
+    image(back_img, 0, 0, 1000, 1000);
+}
+
+
