bb144a8dff1804cf6633cda2055968b6d7015c00
Gregory Eales <gregoeales@aol.com>
2019-05-03 10:34:47 -0700

    finished excercise

--- Java-Script/debugChallengeT2/sketch.js
+++ Java-Script/debugChallengeT2/sketch.js
@@ -7,16 +7,17 @@ function draw()
 {
 	//set the colour and stroke before drawing an ellipse
 	fill(180, 0, 220);
-	strokeWeigth(1);
+	strokeWeight(1);
 	stroke(0);
 
 	ellipse(100, 100, 300, 200);
 
 	//set noFill so the rect isn't a block of colour and set a thicker stroke
 	//so it's more visible.
-	noFill();
-	strokeWeight(4);
+	
+    noFill();
+	strokeWeight(10);
 	stroke(0, 0, 255);
-	Rect(100, 100, 300 200);
-
+	rect(100, 100, 300, 200);
+    
 }
