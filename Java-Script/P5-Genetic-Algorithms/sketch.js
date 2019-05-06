018fac48cae9bcbd4155b2d7054bf9005fb70cd6
Gregory Eales <gregoeales@aol.com>
2019-05-06 10:08:51 -0700

    Update sketch.js

--- Java-Script/P5-Genetic-Algorithms/sketch.js
+++ Java-Script/P5-Genetic-Algorithms/sketch.js
@@ -8,6 +8,8 @@ function setup() {
     createCanvas(windowWidth, windowWidth*2/3);
     scale = windowWidth;
     
+    var org1 = Organism
+    
     // generate population
     
 
@@ -28,38 +30,70 @@ function draw() {
 }
 
 
-function RandomOrganism(){
+class Organism {
+    
+    constructor(color, size, location, weights, energy){
+        // genetics or attributes
+        this.color = color;
+        this.x = location[0];
+        this.y = location[1];
+        this.size = size;
+        this.weights = weights;
+        this.energy = energy;
+    }
+    
+    // Getters
+    
+    get energy(){
+        return this.energy;
+    }
+    
+    get color(){
+        return this.color;
+    }
+    
+    get location(){
+        return this.location;
+    }
+    
+    get weights(){
+        return this.weights;
+    }
+    
+    get size(){
+        return size;
+    }
+    
+    // methods
+    
+    draw(){
+        fill(this.color[0], color[1], color[2]);
+        rect(this.x, this.y, this.size, this.size);
+    }
+}
+
+function GenRandomOrganism(){
     
     // genetics or attributes
     
-    this.color = color
-    this.location = [x, y];
-    this.size = size
+    color = color;
+    location = [x, y];
+    size = size;
+    
+    return org;
     
     // methods or actions
-    
     
 }
 
 
-function Organism(color, size, location){
-    
-    // genetics or attributes
-    
-    this.color = color;
-    this.location = location;
-    this.size = size;
-    this.weights = weights;
-    this.energy = energy
-    
-    // methods or actions
-    
-    
-}
+
 
 
 function generate_offspring(Organsim1, Organism2){
     
     // generate new offspring
 }
+
+
 
